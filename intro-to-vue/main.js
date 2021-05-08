03aeb80a1bd37844a14b388bd30290ba52c8037e
Puneet <birdipuneet@gmail.com>
2021-05-08 14:45:03 -0400

    First commit.

---
+++ intro-to-vue/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "These are the warmest socks your feet have ever experienced.",
+    image:
+      "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+    link:'https://google.com',
+    inventory: 12,
+    onSale: true
+  },
+});
